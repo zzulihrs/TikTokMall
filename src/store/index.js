326bb7c0091f3582d6940ddf45e7e80be343eae7
hrs <2418838198.com>
2025-01-31 23:58:25 +0800

    页面刷新后用户状态将保持不变

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,15 @@ import auth from './modules/auth'
 import cart from './modules/cart'
 import search from './modules/search'
 
-export default createStore({
+const store = createStore({
   modules: {
     auth,
     cart,
     search
   }
 })
+
+// 初始化用户状态
+store.dispatch('auth/initialize')
+
+export default store
