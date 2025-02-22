3d319d47fa74ba240e54981e9f0c60eaa8107d9b
hrs <2418838198@qq.com>
2025-02-22 23:39:32 +0800

    [后端] oss上传图片接口中间件添加auth认证

--- backend/app/frontend/biz/router/oss/middleware.go
+++ backend/app/frontend/biz/router/oss/middleware.go
@@ -4,11 +4,12 @@ package oss
 
 import (
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/tiktokmall/backend/app/frontend/middleware"
 )
 
 func rootMw() []app.HandlerFunc {
 	// your code...
-	return nil
+	return []app.HandlerFunc{middleware.Auth()}
 }
 
 func _uploadimageMw() []app.HandlerFunc {
