d4a8c07d418b14deae28bc0cfb71ccab396013f3
sakura-mac <1760237725@qq.com>
2025-02-28 23:51:58 +0800

    [后端] 更改支付逻辑，只支持alipay且默认个人信息

--- backend/app/agent/pkg/tool/payment/payment.go
+++ backend/app/agent/pkg/tool/payment/payment.go
@@ -24,7 +24,6 @@ const (
 type PaymentRequest struct {
 	Method        PaymentMethod `json:"method" jsonschema:"description=Payment method to use"`
 	UserID        uint32        `json:"user_id" jsonschema:"description=User ID"`
-	TransactionID string        `json:"order_id" jsonschema:"description=Transaction ID to pay for"`
 	Amount        float32       `json:"amount" jsonschema:"description=Payment amount"`
 	CardInfo      *CardInfo     `json:"card_info,omitempty" jsonschema:"description=Card payment details"`
 }
@@ -75,66 +74,24 @@ func (p *PaymentImpl) ToEinoTool() (tool.BaseTool, error) {
 // Invoke 实际执行支付操作
 func (p *PaymentImpl) Invoke(ctx context.Context, req *PaymentRequest) (*PaymentResponse, error) {
 	req.UserID = frontendUtils.GetUserIdFromCtx(ctx)
-	// TODO: 灵活处理transactionid
-	req.TransactionID = "c3332f08-f2aa-11ef-b7d1-525400e93088"
-	// 根据支付方式进行不同处理
-	switch req.Method {
-	case MethodAlipay:
-		return p.handleAlipayPayment(ctx, req)
-	// case MethodWeChat:
-	// 	return p.handleWeChatPayment(ctx, req)
-	// case MethodCard:
-	// 	return p.handleCardPayment(ctx, req)
-	default:
-		return &PaymentResponse{
-			Success: false,
-			Error:   "unsupported payment method",
-		}, nil
+	// 直接使用alipay，默认地址
+	rpc.CheckoutClient.Checkout(ctx, &rpcpayment.CheckoutReq{
+		UserId: req.UserID,
+		Firstname: "test",
+		Lastname: "test",
+		Email: "root@example.com",
+		PaymentMethod: req.Method,
+		Address: &rpcpayment.Address{
+			Country:       "China",
+			ZipCode:       "200000",
+			City:          "Shanghai",
+			State:         "Shanghai",
+			StreetAddress: "test",
+		},}
+	)
+	
+	return &PaymentResponse{
+		Success:     true,
+		PaymentURL: "http://tiktokmall.bhclient.cn/orders"，
 	}
-}
-
-// 处理支付宝支付
-func (p *PaymentImpl) handleAlipayPayment(ctx context.Context, req *PaymentRequest) (*PaymentResponse, error) {
-	payReq := &rpcpayment.AlipayReq{
-		TransactionId: req.TransactionID,
-		TotalAmount:   fmt.Sprintf("%.2f", req.Amount),
-	}
-
-	return p.processPayment(ctx, payReq)
-}
-
-// // 处理微信支付
-// func (p *PaymentImpl) handleWeChatPayment(ctx context.Context, req *PaymentRequest) (*PaymentResponse, error) {
-// 	// 等待后续。。。
-// 	payReq := &rpcpayment.ChargeReq{}
-
-// 	return p.processPayment(ctx, payReq)
-// }
-
-// // 处理银行卡支付
-// func (p *PaymentImpl) handleCardPayment(ctx context.Context, req *PaymentRequest) (*PaymentResponse, error) {
-// 	// 等待后续。。。
-// 	payReq := rpcpayment.ChargeReq{}
-
-// 	return p.processPayment(ctx, payReq)
-// }
-
-// 统一处理支付流程
-func (p *PaymentImpl) processPayment(ctx context.Context, payReq *rpcpayment.AlipayReq) (*PaymentResponse, error) {
-	payResp, err := rpc.PaymentClient.Alipay(ctx, payReq)
-	if err != nil {
-		return &PaymentResponse{
-			Success: false,
-			Error:   fmt.Sprintf("Failed to create payment: %v", err),
-		}, nil
-	}
-
-	resp := &PaymentResponse{}
-
-	// 如果是在线支付方式，设置支付URL
-	// if payReq.Method == string(MethodAlipay) || payReq.Method == string(MethodWeChat) {
-	// 	resp.PaymentURL = payResp.PaymentUrl
-	// }
-	resp.PaymentURL = payResp.GetPayUrl()
-	return resp, nil
 }
--- backend/app/checkout/go.mod
+++ backend/app/checkout/go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/cloudwego/kitex v0.12.1
 	github.com/kitex-contrib/obs-opentelemetry/logging/logrus v0.0.0-20241120035129-55da83caab1b
 	github.com/kr/pretty v0.3.1
-	github.com/nats-io/nats.go v1.38.0
+	github.com/nats-io/nats.go v1.39.1
 	github.com/redis/go-redis/v9 v9.7.0
 	github.com/tiktokmall/backend/common v0.0.0-00010101000000-000000000000
 	github.com/tiktokmall/backend/rpc_gen v0.0.0-00010101000000-000000000000
