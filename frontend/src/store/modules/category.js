3b75dc9fb56cfc33a6334ee93ab2ed6ab90babb0
huiyinian <2352126435@qq.com>
2025-03-02 12:04:15 +0000

    [前端] 类别=All，调用 search?q= 路由

--- frontend/src/store/modules/category.js
+++ frontend/src/store/modules/category.js
@@ -1,40 +1,40 @@
 export default {
-    namespaced: true,
+  namespaced: true,
 
-    state: () => ({
-        category: 'All',
-      categories: [
-        {
-          id: 0,
-          name: 'All',
-          description: 'All'
-        },
-        {
-          id: 1,
-          name: 'T-Shirt',
-          description: 'T-Shirt'
-        },
-        {
-          id: 2,
-          name: 'Sticker',
-          description: 'Sticker'
-        }
-      ]
-    }),
+  state: () => ({
+    category: 'All',
+    categories: [
+      {
+        id: 0,
+        name: 'All',
+        description: 'All'
+      },
+      {
+        id: 1,
+        name: 'T-Shirt',
+        description: 'T-Shirt'
+      },
+      {
+        id: 2,
+        name: 'Sticker',
+        description: 'Sticker'
+      }
+    ]
+  }),
 
-    getters: {
-        // 获取搜索结果
-        categories: state => state.categories,
-        category: state => state.category
-    },
+  getters: {
+    // 获取搜索结果
+    categories: state => state.categories,
+    category: state => state.category
+  },
 
-    mutations: {
-        Update_Category(state, payload) {
-            state.category = payload;
-        }
-    },
-
-    actions: {
-
+  mutations: {
+    Update_Category(state, payload) {
+      state.category = payload;
     }
+  },
+
+  actions: {
+
   }
+}
